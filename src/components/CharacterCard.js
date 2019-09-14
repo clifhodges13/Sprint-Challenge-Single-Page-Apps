f08d0bb0526a0d67fca4f9038d4195ecff858973
Clif <clifhodges13@gmail.com>
2019-09-14 11:49:28 -0500

    had the character cards displaying and refreshed and now getting 'char is undefined' error. working it out now.

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,13 @@
 import React from "react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard(props) {
+  return (
+    <div>
+      <img src={props.char.image} alt={props.char.name} />
+      <h2>{props.char.name}</h2>
+      <p>{props.char.species} / {props.char.status}</p>
+      <p>Origin: {props.char.origin.name}</p>
+      <p>Current Location: {props.char.location.name}</p>
+    </div>
+  );
 }
